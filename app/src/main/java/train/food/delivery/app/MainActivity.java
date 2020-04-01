d71a63fd94f031833229e4c545b29728f92f92b9
HungNP89 <loliology9029@gmail.com>
2020-04-01 19:54:53 +0300

    Listing Restaurant and randon restaurant for each stop

--- app/src/main/java/train/food/delivery/app/MainActivity.java
+++ app/src/main/java/train/food/delivery/app/MainActivity.java
@@ -1,14 +1,30 @@
 package train.food.delivery.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecycleView = (RecyclerView)findViewById(R.id.recycle_restaurantList);
+        new FirebaseDatabaseHelper().readRestaurant(new FirebaseDatabaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<Restaurant> restaurantList, List<String> keys) {
+                new ListRestaurant().setConfig(mRecycleView, MainActivity.this, restaurantList,keys);
+            }
+        });
     }
+
 }
